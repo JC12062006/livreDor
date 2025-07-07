efff9c26abf5f5a858ad1ec71d75af45eac7fe22
Kristof-sZn <siazon.jeanchristophe.pro@gmail.com>
2025-07-07 13:29:06 +0200

    bdd

--- bdd/maj.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE signatures ADD COLUMN Sexe ENUM('Homme', 'Femme', 'Autre') DEFAULT 'Autre',
-ALTER TABLE signatures ADD COLUMN Email VARCHAR(150);
--- modele/signatureModele.php
+++ modele/signatureModele.php
@@ -4,8 +4,6 @@ require_once(__DIR__ . '/../bdd/bdd.php');
 
 /**
  * Récupère toutes les signatures du livre d'or.
- *
- * @return array Tableau associatif contenant les messages triés par date.
  */
 function recupererSignatures() {
     global $bdd;
@@ -18,15 +16,7 @@ function recupererSignatures() {
     return $stmt->fetchAll();
 }
 
-/**
- * Ajoute une nouvelle signature dans la base de données.
- *
- * @param string $nom_auteur Le nom de la personne qui signe.
- * @param string $message    Le contenu du message.
- * @param string $email      Email de la personne.
- * @param string $telephone  Numéro de téléphone.
- * @param string $sexe       Sexe (Homme, Femme, Autre).
- */
+
 function ajouterSignature($nom_auteur, $message, $email, $telephone, $sexe) {
     global $bdd;
 
